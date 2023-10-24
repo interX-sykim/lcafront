e25d504f0cb9500777d7485f20c6975dfbd17997
interX-sykim <sy.kim@interxlab.com>
2023-10-24 13:45:26 +0900

    product 등록 unit list 조회 추가

--- src/pages/ProductRegister.jsx
+++ src/pages/ProductRegister.jsx
@@ -41,8 +41,9 @@ const ProductRegister = () => {
     };
 
     const renderCodeList = codeList.map((code) => {
-        <option>{code.codeName}</option>
-    })
+        return (
+            <option>{code.codeName}</option>
+    )})
 
     const { state, handleChange, handleSubmit } = ProductForm(submit);
     return (
@@ -87,7 +88,6 @@ const ProductRegister = () => {
                         <div class="relative">
                             <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                             {renderCodeList}
-                            <option>lkjfds</option>
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
