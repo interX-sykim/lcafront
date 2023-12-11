5239818471928e991384f5184a6849d0118aaa99
interX-sykim <sy.kim@interxlab.com>
2023-12-11 16:59:13 +0900

    Add turbo custom react flow

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MemberRegister from './pages/MemberRegister';
 import ProductRegister from './pages/ProductRegister';
 import ProcessRegister from './pages/ProcessRegister';
 import ResizablePanel from './component/layer/ResizablePanel';
+import Flow from './processFlow/ProcessFlow';
 
 
 function Root() {
@@ -26,6 +27,7 @@ function Root() {
                     <Route path="/MemberRegister" element={<MemberRegister />} />
                     <Route path="/ProductRegister" element={<ProductRegister />} />
                     <Route path="/ProcessRegister" element={<ProcessRegister />} />
+                    <Route path='/ProcessFlow' element={<Flow/>}/>
                 </Route>
             </Routes>
         </div>);
---
+++ src/processFlow/TurboNode.jsx
@@ -0,0 +1,28 @@
+import React, { memo, ReactNode } from 'react';
+import { Handle, NodeProps, Position } from 'reactflow';
+import { FiCloud } from 'react-icons/fi';
+
+export default memo(({ data }) => {
+  return (
+    <>
+      <div className="cloud gradient">
+        <div>
+          <FiCloud />
+        </div>
+      </div>
+      <div className="wrapper gradient">
+        <div className="inner">
+          <div className="body">
+            {data.icon && <div className="icon">{data.icon}</div>}
+            <div>
+              <div className="title">{data.title}</div>
+              {data.subline && <div className="subline">{data.subline}</div>}
+            </div>
+          </div>
+          <Handle type="target" position={Position.Left} />
+          <Handle type="source" position={Position.Right} />
+        </div>
+      </div>
+    </>
+  );
+});
